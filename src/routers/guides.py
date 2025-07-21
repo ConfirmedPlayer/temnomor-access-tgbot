16e1cbece394a4933a71c3343000396a5c832056
confirmedplayer <confirmedplayer@mail.ru>
2025-07-21 19:18:17 +0500

    added guides

--- src/routers/guides.py
+++ src/routers/guides.py
@@ -1,5 +1,6 @@
 from aiogram import F, Router
 from aiogram.types import CallbackQuery
+
 from core import message_templates as mt
 
 router = Router(name=__name__)
@@ -7,15 +8,23 @@ router = Router(name=__name__)
 
 @router.callback_query(F.data == 'guide_android')
 async def guide_android_query_handler(query: CallbackQuery):
+    await query.bot.send_message(
+        chat_id=query.from_user.id, text=mt.guide_android
+    )
     await query.answer()
 
 
 @router.callback_query(F.data == 'guide_apple')
 async def guide_apple_query_handler(query: CallbackQuery):
-    await query.bot.send_(chat_id=query.from_user.id, text=mt.guide_apple)
+    await query.bot.send_message(
+        chat_id=query.from_user.id, text=mt.guide_apple
+    )
     await query.answer()
 
 
 @router.callback_query(F.data == 'guide_windows')
 async def guide_windows_query_handler(query: CallbackQuery):
+    await query.bot.send_message(
+        chat_id=query.from_user.id, text=mt.guide_windows
+    )
     await query.answer()
