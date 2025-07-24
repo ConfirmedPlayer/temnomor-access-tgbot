2f81c8c118e983c99052ede362dc7e3e2dc1b90d
confirmedplayer <confirmedplayer@mail.ru>
2025-07-24 14:09:06 +0500

    better logging for rate limiter

--- src/tools/scheduler_jobs/subscription_ip_rate_limit.py
+++ src/tools/scheduler_jobs/subscription_ip_rate_limit.py
@@ -13,49 +13,58 @@ async def disallow_simultaneous_connections(
     iteration_sleep_time: Seconds = 3,
     turn_back_on_subscription_after: Minutes = 3,
 ) -> NoReturn:
-    while True:
-        online_clients = await x_ui_session.get_all_online_clients()
-        for online_client in online_clients:
-            ip_addresses = await x_ui_session.get_client_ip_addresses(
-                online_client
-            )
-            if len(ip_addresses) > 1:
-                logger.info(
-                    f'The client "{online_client}" was seen abusing (sharing) the '
-                    f'subscription with others: {ip_addresses}'
+    try:
+        while True:
+            online_clients = await x_ui_session.get_all_online_clients()
+            logger.info(online_clients)
+            for online_client in online_clients:
+                ip_addresses = await x_ui_session.get_client_ip_addresses(
+                    online_client
                 )
-                client_settings = (
-                    await x_ui_session.get_client_settings_by_email(
-                        email=online_client
+                if len(ip_addresses) > 1:
+                    logger.info(
+                        f'The client "{online_client}" was seen abusing (sharing) the '
+                        f'subscription with others: {ip_addresses}'
                     )
-                )
-                await x_ui_session.toggle_subscription_by_uuid(
-                    client_uuid=client_settings.id, enable=False
-                )
+                    client_settings = (
+                        await x_ui_session.get_client_settings_by_email(
+                            email=online_client
+                        )
+                    )
+                    await x_ui_session.toggle_subscription_by_uuid(
+                        client_uuid=client_settings.id, enable=False
+                    )
 
-                telegram_user_id = online_client.split('-')[0]
-                subscription_name = online_client.split('-')[-1]
-                await bot.send_message(
-                    chat_id=telegram_user_id,
-                    text=mt.subscription_was_disabled_abusing.format(
-                        subscription_name
-                    ),
-                )
+                    telegram_user_id = online_client.split('-')[0]
+                    subscription_name = online_client.split('-')[-1]
+                    await bot.send_message(
+                        chat_id=telegram_user_id,
+                        text=mt.subscription_was_disabled_abusing.format(
+                            subscription_name
+                        ),
+                    )
 
-                logger.info('adding abusing job...')
+                    logger.info('adding abusing job...')
 
-                scheduler.add_job(
-                    func=x_ui_session.clear_client_ip_addresses,
-                    kwargs={'email': online_client},
-                    next_run_time=datetime.now()
-                    + timedelta(minutes=turn_back_on_subscription_after - 1),
-                )
+                    scheduler.add_job(
+                        func=x_ui_session.clear_client_ip_addresses,
+                        kwargs={'email': online_client},
+                        next_run_time=datetime.now()
+                        + timedelta(
+                            minutes=turn_back_on_subscription_after - 1
+                        ),
+                    )
 
-                scheduler.add_job(
-                    func=x_ui_session.toggle_subscription_by_uuid,
-                    kwargs={'client_uuid': client_settings.id, 'enable': True},
-                    next_run_time=datetime.now()
-                    + timedelta(minutes=turn_back_on_subscription_after),
-                )
+                    scheduler.add_job(
+                        func=x_ui_session.toggle_subscription_by_uuid,
+                        kwargs={
+                            'client_uuid': client_settings.id,
+                            'enable': True,
+                        },
+                        next_run_time=datetime.now()
+                        + timedelta(minutes=turn_back_on_subscription_after),
+                    )
 
-        await asyncio.sleep(iteration_sleep_time)
+            await asyncio.sleep(iteration_sleep_time)
+    except Exception as _ex:
+        logger.exception(f'Exception in IP Rate Limiter: {_ex}')
