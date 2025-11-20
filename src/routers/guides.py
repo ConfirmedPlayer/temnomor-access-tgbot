bc558716cf8db0bc602545d34369a8d2c3da98ec
confirmedplayer <confirmedplayer@mail.ru>
2025-11-20 20:30:22 +0500

    global guides improvement

--- src/routers/guides.py
+++ src/routers/guides.py
@@ -1,40 +1,38 @@
 from aiogram import F, Router
-from aiogram.types import CallbackQuery, FSInputFile
-from aiogram.utils.media_group import MediaGroupBuilder
+from aiogram.types import CallbackQuery
 
-from core import message_templates as mt
+from core import (
+    message_templates as mt,  # TODO Move hardcoded captions to message templates
+)
 
 router = Router(name=__name__)
 
 
 @router.callback_query(F.data == 'guide_android')
 async def guide_android_query_handler(query: CallbackQuery):
-    media_group_builder = MediaGroupBuilder(caption=mt.guide_android)
-    for i in range(1, 8):
-        media_group_builder.add_photo(FSInputFile(f'./assets/android/{i}.jpg'))
-    await query.bot.send_media_group(
-        chat_id=query.from_user.id, media=media_group_builder.build()
+    await query.bot.send_video(
+        chat_id=query.from_user.id,
+        video='BAACAgIAAxkDAAIEJmkfJdH4uv9-9UnuU3U4goxZSRR5AAJiiAACHbABSYLypmjt8MOmNgQ',
+        caption='Делайте всё чётко по инструкции в видео и у вас всё получится!',
     )
     await query.answer()
 
 
 @router.callback_query(F.data == 'guide_apple')
 async def guide_apple_query_handler(query: CallbackQuery):
-    media_group_builder = MediaGroupBuilder(caption=mt.guide_apple)
-    for i in range(1, 6):
-        media_group_builder.add_photo(FSInputFile(f'./assets/ios/{i}.jpg'))
-    await query.bot.send_media_group(
-        chat_id=query.from_user.id, media=media_group_builder.build()
+    await query.bot.send_video(
+        chat_id=query.from_user.id,
+        video='BAACAgIAAxkDAAIEF2kfILBeYgk17_nY-AQU4BmJmL1aAALrhwACHbABSc_TgJfvZ8cDNgQ',
+        caption='Делайте всё чётко по инструкции в видео и у вас всё получится!',
     )
     await query.answer()
 
 
 @router.callback_query(F.data == 'guide_windows')
 async def guide_windows_query_handler(query: CallbackQuery):
-    media_group_builder = MediaGroupBuilder(caption=mt.guide_windows)
-    for i in range(1, 8):
-        media_group_builder.add_photo(FSInputFile(f'./assets/windows/{i}.jpg'))
-    await query.bot.send_media_group(
-        chat_id=query.from_user.id, media=media_group_builder.build()
+    await query.bot.send_video(
+        chat_id=query.from_user.id,
+        video='BAACAgIAAxkDAAIEMWkfMxtAf4UL2qA9GhUK7Iit1F4KAAKGiQACHbABSYEcfVEs8MyDNgQ',
+        caption='Ссылка на скачивание: https://github.com/hiddify/hiddify-app\n\nДелайте всё чётко по инструкции в видео и у вас всё получится!',
     )
     await query.answer()
