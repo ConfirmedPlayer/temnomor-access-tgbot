e61fefb7e03805025d05bbf0a7cb37199f8603a3
confirmedplayer <confirmedplayer@mail.ru>
2025-11-20 20:46:34 +0500

    back to bc558716cf8db0bc602545d34369a8d2c3da98ec

--- src/routers/guides.py
+++ src/routers/guides.py
@@ -1,5 +1,5 @@
 from aiogram import F, Router
-from aiogram.types import CallbackQuery, FSInputFile
+from aiogram.types import CallbackQuery
 
 from core import (
     message_templates as mt,  # TODO Move hardcoded captions to message templates
@@ -10,44 +10,29 @@ router = Router(name=__name__)
 
 @router.callback_query(F.data == 'guide_android')
 async def guide_android_query_handler(query: CallbackQuery):
-    video_file = FSInputFile('./assets/android/android_guide.mp4')
-    a = await query.bot.send_video(
+    await query.bot.send_video(
         chat_id=query.from_user.id,
-        video=video_file,#'BAACAgIAAxkDAAIEJmkfJdH4uv9-9UnuU3U4goxZSRR5AAJiiAACHbABSYLypmjt8MOmNgQ',
+        video='BAACAgIAAxkDAAIEJmkfJdH4uv9-9UnuU3U4goxZSRR5AAJiiAACHbABSYLypmjt8MOmNgQ',
         caption='Делайте всё чётко по инструкции в видео и у вас всё получится!',
-    )
-    await query.bot.send_message(
-        chat_id=query.from_user.id,
-        text=a.video.file_id
     )
     await query.answer()
 
 
 @router.callback_query(F.data == 'guide_apple')
 async def guide_apple_query_handler(query: CallbackQuery):
-    video_file = FSInputFile('./assets/ios/ios_macos_guide.mp4')
-    a = await query.bot.send_video(
+    await query.bot.send_video(
         chat_id=query.from_user.id,
-        video=video_file,#'BAACAgIAAxkDAAIEJmkfJdH4uv9-9UnuU3U4goxZSRR5AAJiiAACHbABSYLypmjt8MOmNgQ',
+        video='BAACAgIAAxkDAAIEF2kfILBeYgk17_nY-AQU4BmJmL1aAALrhwACHbABSc_TgJfvZ8cDNgQ',
         caption='Делайте всё чётко по инструкции в видео и у вас всё получится!',
-    )
-    await query.bot.send_message(
-        chat_id=query.from_user.id,
-        text=a.video.file_id
     )
     await query.answer()
 
 
 @router.callback_query(F.data == 'guide_windows')
 async def guide_windows_query_handler(query: CallbackQuery):
-    video_file = FSInputFile('./assets/windows/windows_guide.mp4')
-    a = await query.bot.send_video(
+    await query.bot.send_video(
         chat_id=query.from_user.id,
-        video=video_file,#'BAACAgIAAxkDAAIEJmkfJdH4uv9-9UnuU3U4goxZSRR5AAJiiAACHbABSYLypmjt8MOmNgQ',
-        caption='Делайте всё чётко по инструкции в видео и у вас всё получится!',
-    )
-    await query.bot.send_message(
-        chat_id=query.from_user.id,
-        text=a.video.file_id
+        video='BAACAgIAAxkDAAIEMWkfMxtAf4UL2qA9GhUK7Iit1F4KAAKGiQACHbABSYEcfVEs8MyDNgQ',
+        caption='Ссылка на скачивание: https://github.com/hiddify/hiddify-app\n\nДелайте всё чётко по инструкции в видео и у вас всё получится!',
     )
     await query.answer()
