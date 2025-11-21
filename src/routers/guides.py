a5181ad446972da0fa063d6a145c39416399b4d1
confirmedplayer <confirmedplayer@mail.ru>
2025-11-21 18:23:13 +0500

    organazied message templates

--- src/routers/guides.py
+++ src/routers/guides.py
@@ -1,9 +1,7 @@
 from aiogram import F, Router
-from aiogram.types import CallbackQuery, FSInputFile
+from aiogram.types import CallbackQuery
 
-from core import (
-    message_templates as mt,  # TODO Move hardcoded captions to message templates
-)
+from core import message_templates as mt
 
 router = Router(name=__name__)
 
@@ -13,7 +11,7 @@ async def guide_android_query_handler(query: CallbackQuery):
     await query.bot.send_video(
         chat_id=query.from_user.id,
         video='BAACAgIAAxkDAAIFbWkfNutSRQs6cQmOkU9No1OYYyAhAAIbiwACQggAAUmxT6QKnf8ewDYE',
-        caption='Делайте всё чётко по инструкции в видео и у вас всё получится!',
+        caption=mt.guide_caption,
     )
     await query.answer()
 
@@ -23,7 +21,7 @@ async def guide_apple_query_handler(query: CallbackQuery):
     await query.bot.send_video(
         chat_id=query.from_user.id,
         video='BAACAgIAAxkDAAIFb2kfNvemorkeNNQAARM0d2SHHeNBsQACHIsAAkIIAAFJAl2YsH1Dsmo2BA',
-        caption='Делайте всё чётко по инструкции в видео и у вас всё получится!',
+        caption=mt.guide_caption,
     )
     await query.answer()
 
@@ -33,6 +31,6 @@ async def guide_windows_query_handler(query: CallbackQuery):
     await query.bot.send_video(
         chat_id=query.from_user.id,
         video='BAACAgIAAxkDAAIFgmkfSJ7gmkgSyhAmExmbNX4NJFZ_AAKAjAACQggAAUkrT0ad2rwm3jYE',
-        caption='Ссылка на скачивание: https://github.com/hiddify/hiddify-app\n\nДелайте всё чётко по инструкции в видео и у вас всё получится!\n\n<b>ОБЯЗАТЕЛЬНО запускайте программу от имени Администратора!</b>',
+        caption=mt.guide_caption_windows,
     )
     await query.answer()
