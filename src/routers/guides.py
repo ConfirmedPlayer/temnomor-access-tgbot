9ffd5c6b70c119a85b9f762329e09e9c9170b2e0
confirmedplayer <confirmedplayer@mail.ru>
2025-11-27 17:08:05 +0500

    temporal commit

--- src/routers/guides.py
+++ src/routers/guides.py
@@ -1,5 +1,5 @@
 from aiogram import F, Router
-from aiogram.types import CallbackQuery
+from aiogram.types import CallbackQuery, FSInputFile
 
 from core import message_templates as mt
 
@@ -8,20 +8,30 @@ router = Router(name=__name__)
 
 @router.callback_query(F.data == 'guide_android')
 async def guide_android_query_handler(query: CallbackQuery):
-    await query.bot.send_video(
+    video_file = FSInputFile('./assets/android/android_guide.mp4')
+    a = await query.bot.send_video(
         chat_id=query.from_user.id,
-        video='BAACAgIAAxkDAAIFbWkfNutSRQs6cQmOkU9No1OYYyAhAAIbiwACQggAAUmxT6QKnf8ewDYE',
+        video=video_file,
         caption=mt.guide_caption,
+    )
+    await query.bot.send_message(
+        chat_id=query.from_user.id,
+        text=a.video.file_id
     )
     await query.answer()
 
 
 @router.callback_query(F.data == 'guide_apple')
 async def guide_apple_query_handler(query: CallbackQuery):
-    await query.bot.send_video(
+    video_file = FSInputFile('./assets/ios/ios_macos_guide.mp4')
+    a = await query.bot.send_video(
         chat_id=query.from_user.id,
-        video='BAACAgIAAxkDAAIFb2kfNvemorkeNNQAARM0d2SHHeNBsQACHIsAAkIIAAFJAl2YsH1Dsmo2BA',
+        video=video_file,
         caption=mt.guide_caption,
+    )
+    await query.bot.send_message(
+        chat_id=query.from_user.id,
+        text=a.video.file_id
     )
     await query.answer()
 
