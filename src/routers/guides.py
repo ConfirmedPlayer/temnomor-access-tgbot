643d42a0d14f766658f7c5493f6004f7ce3cf588
confirmedplayer <confirmedplayer@mail.ru>
2025-11-28 15:06:06 +0500

    mirror changes

--- src/routers/guides.py
+++ src/routers/guides.py
@@ -6,11 +6,16 @@ from core import message_templates as mt
 router = Router(name=__name__)
 
 
+ANDROID_VIDEOGUIDE_TELEGRAM_FILE_ID = 'BAACAgIAAxkDAAIGbmkoSVUKRiXnSiS_Z5KdU6vyWbEcAAKhhgACTKZASXTmqBDtZYIBNgQ'
+APPLE_VIDEOGUIDE_TELEGRAM_FILE_ID = 'BAACAgIAAxkDAAIGbGkoSOgQtUIfBBuBYJP9x7pqRQ9qAAKbhgACTKZASed4cE54lQAB5zYE'
+WINDOWS_VIDEOGUIDE_TELEGRAM_FILE_ID = 'BAACAgIAAxkDAAIFymkid51O4V_dq9yl1gAB1EVjuUlH2wACNY0AApbYEUny-pLBIMJBTTYE'
+
+
 @router.callback_query(F.data == 'guide_android')
 async def guide_android_query_handler(query: CallbackQuery):
     await query.bot.send_video(
         chat_id=query.from_user.id,
-        video='BAACAgIAAxkDAAIGbmkoSVUKRiXnSiS_Z5KdU6vyWbEcAAKhhgACTKZASXTmqBDtZYIBNgQ',
+        video=ANDROID_VIDEOGUIDE_TELEGRAM_FILE_ID,
         caption=mt.guide_caption_android,
     )
     await query.answer()
@@ -20,8 +25,8 @@ async def guide_android_query_handler(query: CallbackQuery):
 async def guide_apple_query_handler(query: CallbackQuery):
     await query.bot.send_video(
         chat_id=query.from_user.id,
-        video='BAACAgIAAxkDAAIGbGkoSOgQtUIfBBuBYJP9x7pqRQ9qAAKbhgACTKZASed4cE54lQAB5zYE',
-        caption=mt.guide_caption_ios,
+        video=APPLE_VIDEOGUIDE_TELEGRAM_FILE_ID,
+        caption=mt.guide_caption_apple,
     )
     await query.answer()
 
@@ -30,7 +35,7 @@ async def guide_apple_query_handler(query: CallbackQuery):
 async def guide_windows_query_handler(query: CallbackQuery):
     await query.bot.send_video(
         chat_id=query.from_user.id,
-        video='BAACAgIAAxkDAAIFymkid51O4V_dq9yl1gAB1EVjuUlH2wACNY0AApbYEUny-pLBIMJBTTYE',
+        video=WINDOWS_VIDEOGUIDE_TELEGRAM_FILE_ID,
         caption=mt.guide_caption_windows,
     )
     await query.answer()
