2ccd926edeb8affc2936708583c6e3d2b4778c4e
confirmedplayer <confirmedplayer@mail.ru>
2025-11-28 15:05:56 +0500

    improved text messages

--- src/core/message_templates.py
+++ src/core/message_templates.py
@@ -106,10 +106,10 @@ guide_caption_android = (
     'Ссылка на скачивание приложения: https://github.com/2dust/v2rayNG/releases/latest'
 )
 
-guide_caption_ios = (
+guide_caption_apple = (
     'Делайте всё чётко по инструкции в видео и у вас всё получится!\n'
-    'Инструкция на iOS и MacOS полностью аналогична.\n\n'
-    'Ссылка на скачивание: https://apps.apple.com/kz/app/happ-proxy-utility/id6504287215'
+    'Инструкция на iOS и MacOS (iPhone, MacBook) полностью аналогична.\n\n'
+    'Ссылка на скачивание (AppStore), приложение "Happ": https://apps.apple.com/kz/app/happ-proxy-utility/id6504287215'
 )
 
 guide_caption_windows = 'Ссылка на скачивание: https://github.com/throneproj/Throne\n\nДелайте всё чётко по инструкции в видео и у вас всё получится!'
